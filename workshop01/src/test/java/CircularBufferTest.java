ab5867ab7a98fbd7351804c0666e2d4f8a7e9276
t-wannapa <wannapa.sien@gmail.com>
2025-06-08 22:35:45 +0700

    test #1
    test #2
    
    test #3

--- workshop01/src/test/java/CircularBufferTest.java
+++ workshop01/src/test/java/CircularBufferTest.java
@@ -26,6 +26,6 @@ class CircularBufferTest {
         int size = circularBuffer.getSize();
 
         // Validate/Checking/Assert
-        assertEquals(10, size);
+        assertEquals(8, size);
     }
 }
